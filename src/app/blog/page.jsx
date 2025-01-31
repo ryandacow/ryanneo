b369054e25ab4e40b5bba44794c7a17d7ebafd2e
ryan neo jia hao <ryandacow@ryans-MacBook-Air.local>
2025-01-31 13:50:51 +0800

    Fixed Blogcard Component import

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import BlogCard from "../components/BlogCard";
+import BlogCard from "../Components/BlogCard";
 import Link from "next/link";
 
 const Blog = () => {
